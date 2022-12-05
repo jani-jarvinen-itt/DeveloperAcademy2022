5e96d11f8eedb5b49ae59c950a3dd98236358da4
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-05 15:21:09 +0200

    Added unit tests to the discount calculation logic.

--- DotNet/GoodProgramming/Program.cs
+++ DotNet/GoodProgramming/Program.cs
@@ -9,3 +9,28 @@ Console.WriteLine(result); // 427,5
 CustomerDiscountManager better = new();
 result = better.CalculateCustomerPrice(500, 2, 7);
 Console.WriteLine(result); // 427,5
+
+
+// testing code
+result = better.CalculateCustomerPrice(500, 1, 0);
+double expected = 500.0;
+if (result == expected)
+{
+    Console.WriteLine("Test 1 succeeded.");
+}
+else
+{
+    Console.WriteLine("Test 1 FAILED!");
+}
+
+// test case 2
+result = better.CalculateCustomerPrice(100, 2, 0);
+expected = 90.0;
+if (result == expected)
+{
+    Console.WriteLine("Test 2 succeeded.");
+}
+else
+{
+    Console.WriteLine("Test 2 FAILED!");
+}
---
+++ DotNet/GoodProgrammingTests/CustomerDiscountManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BadProgramming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Metrics;
+
+namespace BadProgramming.Tests
+{
+    [TestClass()]
+    public class CustomerDiscountManagerTests
+    {
+        [TestMethod()]
+        public void CalculateCustomerPriceTest_NormalTier()
+        {
+            CustomerDiscountManager discount = new();
+            double result = discount.CalculateCustomerPrice(500, 1, 0);
+            double expected = 500.0;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void CalculateCustomerPriceTest_SilverTier()
+        {
+            CustomerDiscountManager discount = new();
+            double result = discount.CalculateCustomerPrice(100, 2, 0);
+            double expected = 90.0;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
