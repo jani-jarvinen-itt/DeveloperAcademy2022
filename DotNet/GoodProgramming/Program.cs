d80e72092fea1cbb666ba65011d0e65e0f4de17a
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-05 13:35:49 +0200

    Added project for good programming examples.

---
+++ DotNet/GoodProgramming/Program.cs
@@ -0,0 +1,11 @@
+﻿using BadProgramming;
+
+// original implementation
+Class1 bad = new();
+double result = bad.Calculate(500, 2, 7);
+Console.WriteLine(result); // 427,5
+
+// improved implementation
+CustomerDiscountManager better = new();
+result = better.CalculateCustomerPrice(500, 2, 7);
+Console.WriteLine(result); // 427,5
