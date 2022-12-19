4ba2e0c16330d09337727d22cf3d51b637b97464
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-19 10:27:44 +0200

    Added calculation logic.

--- HTML/Calculator.js
+++ HTML/Calculator.js
@@ -1,5 +1,11 @@
 function calculateSum() {
     console.log("Button clicked!");
+
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+
+    let sum = parseInt(number1) + parseInt(number2);
+    alert("Sum on the numbers is: " + sum);
 }
 
 console.log("Starting JavaScript calculator demo.");
