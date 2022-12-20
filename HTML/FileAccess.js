3156fd0eb3a58172e88ce916469a51a3813e04a5
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-20 09:49:20 +0200

    Added a (non-working) file access demo.

---
+++ HTML/FileAccess.js
@@ -0,0 +1,20 @@
+console.log("Hello, this is FileAccess.js!");
+
+const fs = require('fs');
+const readline = require('readline');
+
+const file = readline.createInterface({
+    input: fs.createReadStream('Numbers.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+let sum = 0;
+file.on('line', (line) => {
+    // console.log(line);
+    sum += parseInt(line);
+});
+
+file.on('close', () => {
+    console.log("Sum of numbers: " + sum);
+});
