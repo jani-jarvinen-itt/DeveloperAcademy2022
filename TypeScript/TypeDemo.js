df37cf61d6798a51e53374f24f2e359a4039c6ec
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-20 11:35:13 +0200

    Added a simple TypeScript typing demo.

---
+++ TypeScript/TypeDemo.js
@@ -0,0 +1,8 @@
+function myDemo(a, b) {
+    console.log(a + b);
+}
+myDemo(1, 2);
+// myDemo(1, "2");
+// myDemo(1);
+// let myNumber: number = "ABCD";
+var myNumber = 1234;
---
+++ TypeScript/TypeDemo.ts
@@ -0,0 +1,10 @@
+function myDemo(a: number, b: number) {
+    console.log(a + b);
+}
+
+myDemo(1, 2);
+// myDemo(1, "2");
+// myDemo(1);
+
+// let myNumber: number = "ABCD";
+let myNumber: number = 1234;
