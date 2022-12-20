7de244cd744601d7b446c44386dd82283da88da7
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-20 10:19:21 +0200

    Added code to log the individual user names to the console.

--- HTML/FetchApiDemo.js
+++ HTML/FetchApiDemo.js
@@ -2,7 +2,14 @@ function fetchUsers() {
 
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(users => {
+            console.log(users);
+
+            for (let index = 0; index < users.length; index++) {
+                const user = users[index];
+                console.log(user.name);
+            }
+        });
 }
 
 console.log("Starting the Fetch API demo!");
