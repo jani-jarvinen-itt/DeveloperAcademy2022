d615d07ffa1027abd00443a8b6610fe276093ca6
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-20 13:54:57 +0200

    Added simple table generation logic on the fly.

---
+++ TypeScript/DomDemo.js
@@ -0,0 +1,30 @@
+function addTable() {
+    // sample data
+    var persons = [
+        {
+            name: "John Doe",
+            age: 35
+        },
+        {
+            name: "Jane Doe",
+            age: 33
+        }
+    ];
+    // create HTML table from the persons
+    var tableHtml = "<table><thead><tr><th>Number</th>" +
+        "<th>Name</th><th>Age</th></tr></thead><tbody>";
+    for (var index = 0; index < persons.length; index++) {
+        var person = persons[index];
+        var tableRow = "<tr><td>".concat(index + 1, "</td>") +
+            "<td>".concat(person.name, "</td>") +
+            "<td>".concat(person.age, "</td></tr>");
+        tableHtml += tableRow;
+    }
+    tableHtml += "</tbody></table>";
+    // show the table in the user interface
+    var results = document.getElementById("myDivArea");
+    if (results) {
+        results.innerHTML = tableHtml;
+    }
+}
+console.log("Starting the DOM demo in TypeScript!");
--- TypeScript/DomDemo.ts
+++ TypeScript/DomDemo.ts
@@ -5,6 +5,7 @@ interface Person {
 
 function addTable(): void {
 
+    // sample data
     const persons: Person[] = [
         {
             name: "John Doe",
@@ -15,6 +16,27 @@ function addTable(): void {
             age: 33
         }
     ];
+
+    // create HTML table from the persons
+    let tableHtml: string = "<table><thead><tr><th>Number</th>" +
+        "<th>Name</th><th>Age</th></tr></thead><tbody>";
+
+    for (let index: number = 0; index < persons.length; index++) {
+        const person: Person = persons[index];
+
+        const tableRow: string = `<tr><td>${index + 1}</td>` +
+            `<td>${person.name}</td>` +
+            `<td>${person.age}</td></tr>`;
+        tableHtml += tableRow;
+    }
+
+    tableHtml += "</tbody></table>";
+
+    // show the table in the user interface
+    const results = document.getElementById("myDivArea");
+    if (results) {
+        results.innerHTML = tableHtml;
+    }
 }
 
 console.log("Starting the DOM demo in TypeScript!");
