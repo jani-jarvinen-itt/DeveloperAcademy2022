7c369e55e1b26759407bea85664d8e7f0ff243c8
Jani Järvinen <jani.jarvinen@intertechno.org>
2022-12-20 10:27:54 +0200

    Added functionality to list the user names on the user interface.

--- HTML/FetchApiDemo.js
+++ HTML/FetchApiDemo.js
@@ -5,10 +5,16 @@ function fetchUsers() {
         .then(users => {
             console.log(users);
 
+            let userNames = "";
             for (let index = 0; index < users.length; index++) {
                 const user = users[index];
                 console.log(user.name);
+
+                userNames += user.name + "\r\n";
             }
+
+            // show the user names in the user interface
+            document.getElementById("resultsArea").innerText = userNames;
         });
 }
 
