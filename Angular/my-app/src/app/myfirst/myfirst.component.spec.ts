3076c85e4690d1b5115d6d4e70a4ce79b47eefa5
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-02 11:22:01 +0200

    Added a single new Angular component.

---
+++ Angular/my-app/src/app/myfirst/myfirst.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyfirstComponent } from './myfirst.component';
+
+describe('MyfirstComponent', () => {
+  let component: MyfirstComponent;
+  let fixture: ComponentFixture<MyfirstComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyfirstComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyfirstComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
