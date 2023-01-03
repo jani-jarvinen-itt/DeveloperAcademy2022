4a0ef1e037421371417db3288334b0056239f8a9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-03 11:21:09 +0200

    Added the first React component.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './First';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, React!</h1>
+        <First />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ React/my-app/src/First.js
@@ -0,0 +1,5 @@
+function First() {
+  return <h1>Hello, this is the first component!</h1>;
+}
+
+export default First;
