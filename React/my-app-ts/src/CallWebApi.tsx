d23498aa8a523e452746f2515b14c34ea10db306
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-03 14:50:00 +0200

    Added React component button click event handler.

--- React/my-app-ts/src/CallWebApi.tsx
+++ React/my-app-ts/src/CallWebApi.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class CallWebApi extends React.Component {
+
+    handleButtonClick() {
+        console.log("Button clicked, it works!");
+
+        console.log("Starting to call the Express.js API.");
+        fetch('http://localhost:3300/')
+            .then(response => response.json())
+            .then(data => {
+                console.log("Express.js API has been called.");
+                console.log(data);
+                // document.getElementById("resultsArea").innerText = data.myproperty;
+            });
+    }
+
     render() {
         return <div>
             <h1>CallWebApi</h1>
-            <button>My button</button>
+            <button onClick={this.handleButtonClick}>My button</button>
         </div>;
     }
 }
