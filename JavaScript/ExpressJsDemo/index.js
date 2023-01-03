41d0faf7de094252f25df531391ed9b350911b3a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-03 09:04:22 +0200

    Added examples on how to manipulate HTTP headers in responses and requests.

--- JavaScript/ExpressJsDemo/index.js
+++ JavaScript/ExpressJsDemo/index.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+}));
+
 app.get('/', (req, res) => {
     res.type("json");
+    res.setHeader("X-My-Own-Header", "Testing123");
     res.send('{ "myproperty": "abcd" }');
 });
 
 app.get('/another', (req, res) => {
+
+    const headerName = "MySuperSecret";
+    console.log(req.headers);
+    let secretValue = req.header(headerName);
     res.type("json");
-    res.send('{ "anotherProperty": 1234 }');
+
+    if (secretValue == "abcd1234") {
+        console.log("Header OK.");
+        res.send('{ "anotherProperty": 1234 }');
+    }
+    else {
+        console.log("Header invalid.");
+        res.send('{ "anotherProperty": "invalid header value" }');
+    }
 });
 
 app.listen(port, () => {
