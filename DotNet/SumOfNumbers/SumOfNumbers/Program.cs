8a4a24cbbdc8065780f237410aff294324e98050
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-25 13:40:03 +0200

    Added first version of number sum calculation application with some exception handling.

---
+++ DotNet/SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿string filename = "C:\\DeveloperAcademy\\Code\\JavaScript\\Numbers.txt";
+string[] lines = File.ReadAllLines(filename);
+
+int sum = 0;
+try
+{
+    foreach (string line in lines)
+    {
+        int number = int.Parse(line);
+        sum += number;
+    }
+}
+catch
+{
+    Console.WriteLine("Error while processing numbers file.");
+}
+
+Console.WriteLine(sum);
