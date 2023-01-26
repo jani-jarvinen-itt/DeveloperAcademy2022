a063015660817f43ea76d8ba0b78aa3c0f439003
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-26 11:22:31 +0200

    Started ASP.NET MVC application for user listing in a HTML table.

---
+++ DotNet/AspNetMvcUserTable/AspNetMvcUserTable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMvcUserTable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
