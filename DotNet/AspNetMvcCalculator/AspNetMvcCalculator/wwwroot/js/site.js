4fdd09b1d503b858ee02ad232e6d2082a8b56943
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-26 10:17:23 +0200

    Added calculator user interface and JavaScript logic.

--- DotNet/AspNetMvcCalculator/AspNetMvcCalculator/wwwroot/js/site.js
+++ DotNet/AspNetMvcCalculator/AspNetMvcCalculator/wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function calculateSum() {
+    console.log("Button clicked!");
+
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+
+    let sum = parseInt(number1) + parseInt(number2);
+    alert("Sum on the numbers is: " + sum);
+}
+
+console.log("Starting ASP.NET MVC calculator demo.");
