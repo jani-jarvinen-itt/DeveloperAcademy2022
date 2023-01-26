26b5dbb780a2c37f0c955972459b168bdb9be0c9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-01-26 09:48:46 +0200

    Started implementing a calculator with ASP.NET MVC technologies.

---
+++ DotNet/AspNetMvcCalculator/AspNetMvcCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMvcCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
