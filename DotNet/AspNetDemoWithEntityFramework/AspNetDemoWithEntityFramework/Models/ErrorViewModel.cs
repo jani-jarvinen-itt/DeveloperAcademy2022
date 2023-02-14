74394d96dbe6ee8a4b993062c2a3a5ac84fc2812
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-02-14 15:43:55 +0200

    Starting point for ASP.NET MVC + EF demo.

---
+++ DotNet/AspNetDemoWithEntityFramework/AspNetDemoWithEntityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetDemoWithEntityFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
