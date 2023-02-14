69f869452bf960441795086a10fc5e8e5fca8707
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-02-14 15:44:18 +0200

    Added Entity Framework demo code.

--- DotNet/EntityDemo2/EntityDemo2/Program.cs
+++ DotNet/EntityDemo2/EntityDemo2/Program.cs
@@ -8,10 +8,22 @@ int numberOfCustomers = context.Customers.Count();
 Console.WriteLine(numberOfCustomers);
 */
 
+/*
+// Warning: fetches all customer rows to the client C# application
 foreach (var customer in context.Customers)
 {
     if (customer.Country == "Finland")
     {
         Console.WriteLine(customer.CompanyName);
     }
+}
+*/
+
+// LINQ query to the Northwind database
+var finnishCustomer = (from c in context.Customers
+                       where c.Country == "Finland"
+                       select c).ToList();
+foreach (var customer in finnishCustomer)
+{
+    Console.WriteLine(customer.CompanyName);
 }
