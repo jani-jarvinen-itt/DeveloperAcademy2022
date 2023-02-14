fb4ab741b22d56968d5095b0303846113a21514c
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-02-14 11:45:46 +0200

    Added exception handling and resource housekeeping.

--- DotNet/NorthwindDemo/NorthwindDemo/Program.cs
+++ DotNet/NorthwindDemo/NorthwindDemo/Program.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.Data.SqlClient;
 
 // open the connection to the Northwind database
-Console.WriteLine("Starting to connect to the Northwind database.");
-string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
-SqlConnection connection = new(connectionString);
-connection.Open();
-Console.WriteLine("Connection open!");
-
-// execute an SQL query against the database
-string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
-SqlCommand command = new(sql, connection);
-
-SqlDataReader reader = command.ExecuteReader();
-while (reader.Read())
+try
 {
-    Console.WriteLine(reader["CompanyName"]);
-}
+	Console.WriteLine("Starting to connect to the Northwind database.");
+	string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
+	using SqlConnection connection = new(connectionString);
+	connection.Open();
+	Console.WriteLine("Connection open!");
 
-Console.WriteLine("---------");
-Console.WriteLine("Data has been read.");
+	// execute an SQL query against the database
+	string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+    using SqlCommand command = new(sql, connection);
+
+    // read the customer data
+    using SqlDataReader reader = command.ExecuteReader();
+	while (reader.Read())
+	{
+		Console.WriteLine(reader.GetString(1)); // second column
+		Console.WriteLine(reader["CompanyName"]);
+	}
+	Console.WriteLine("---------");
+	Console.WriteLine("Data has been read.");
+
+	/*
+	// housekeeping
+	reader.Close();
+	command.Dispose();
+	connection.Close();
+	*/
+}
+catch (Exception ex)
+{
+	Console.WriteLine("Cannot access database: " + ex.Message);
+}
