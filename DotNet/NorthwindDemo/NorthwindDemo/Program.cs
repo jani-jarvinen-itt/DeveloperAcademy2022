5916347128dd4697b673511f5a3179a1fb4a90ac
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-02-14 11:31:41 +0200

    Added NuGet packages and and wrote first version of Northwind database access.

--- DotNet/NorthwindDemo/NorthwindDemo/Program.cs
+++ DotNet/NorthwindDemo/NorthwindDemo/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+// open the connection to the Northwind database
+Console.WriteLine("Starting to connect to the Northwind database.");
+string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
+SqlConnection connection = new(connectionString);
+connection.Open();
+Console.WriteLine("Connection open!");
+
+// execute an SQL query against the database
+string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+SqlCommand command = new(sql, connection);
+
+SqlDataReader reader = command.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine(reader["CompanyName"]);
+}
+
+Console.WriteLine("---------");
+Console.WriteLine("Data has been read.");
