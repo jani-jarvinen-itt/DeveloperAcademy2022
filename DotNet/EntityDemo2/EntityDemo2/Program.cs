af6f6dcee615d15b98dd6e59ce017faa55a6175c
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-02-14 14:47:13 +0200

    Added two Entity Framework demos.

---
+++ DotNet/EntityDemo2/EntityDemo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using EntityDemo2.Models;
+
+Console.WriteLine("Starting Entity Framework Demo 2.");
+
+NorthwindContext context = new();
+/*
+int numberOfCustomers = context.Customers.Count();
+Console.WriteLine(numberOfCustomers);
+*/
+
+foreach (var customer in context.Customers)
+{
+    if (customer.Country == "Finland")
+    {
+        Console.WriteLine(customer.CompanyName);
+    }
+}
