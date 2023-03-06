c17f94f8e7a478335497f565cab5488fe95488a9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-06 11:36:21 +0200

    Started added a new API controller for customer data.

---
+++ DotNet/AspNetWebApiWithEntities/AspNetWebApiWithEntities/Controllers/CustomersApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiWithEntities.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersApiController : ControllerBase
+    {
+        /*
+        public string SayHello()
+        {
+            return "Hello, from ASP.NET Web API!";
+        }
+
+        public string SayHello2() => "Hello, from ASP.NET Web API!";
+        */
+    }
+}
