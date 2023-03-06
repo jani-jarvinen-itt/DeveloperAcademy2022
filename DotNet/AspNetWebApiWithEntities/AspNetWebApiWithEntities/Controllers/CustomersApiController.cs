71954226d0792ed25ec847bb1da8e5cac0993157
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-06 13:47:32 +0200

    Implemented Customer table CRUD operations using Web API.

--- DotNet/AspNetWebApiWithEntities/AspNetWebApiWithEntities/Controllers/CustomersApiController.cs
+++ DotNet/AspNetWebApiWithEntities/AspNetWebApiWithEntities/Controllers/CustomersApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AspNetWebApiWithEntities.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetWebApiWithEntities.Controllers
@@ -15,5 +16,75 @@ namespace AspNetWebApiWithEntities.Controllers
 
         public string SayHello2() => "Hello, from ASP.NET Web API!";
         */
+
+        // R = read
+        [HttpGet]
+        [Route("")]
+        public List<Customer> GetAll()
+        {
+            NorthwindContext context = new();
+            return context.Customers.ToList();
+        }
+
+        // C = create
+        [HttpPost]
+        [Route("")]
+        public bool AddNew(Customer customer)
+        {
+            NorthwindContext context = new();
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+            return true;
+        }
+
+        // U = update
+        [HttpPut]
+        [Route("")]
+        public bool UpdateExisting(Customer customer)
+        {
+            NorthwindContext context = new();
+
+            // search for an existing customer
+            string customerId = customer.CustomerId;
+            Customer? existingCustomer =
+                context.Customers.FirstOrDefault(c => c.CustomerId == customerId);
+            if (existingCustomer is null)
+            {
+                throw new Exception("Customer id was not found.");
+            }
+
+            // update customer fields
+            existingCustomer.CompanyName = customer.CompanyName;
+            existingCustomer.ContactName = customer.ContactName;
+            existingCustomer.ContactTitle = customer.ContactTitle;
+
+            // save to database
+            context.SaveChanges();
+            return true;
+        }
+
+        // D = delete
+        [HttpDelete]
+        [Route("{customerId}")]
+        public bool DeleteExisting(string customerId)
+        {
+            NorthwindContext context = new();
+
+            // search for an existing customer
+            Customer? existingCustomer =
+                context.Customers.FirstOrDefault(c => c.CustomerId == customerId);
+            if (existingCustomer is null)
+            {
+                throw new Exception("Customer id was not found.");
+            }
+
+            // delete the customer
+            context.Customers.Remove(existingCustomer);
+
+            // save to database
+            context.SaveChanges();
+            return true;
+        }
     }
 }
