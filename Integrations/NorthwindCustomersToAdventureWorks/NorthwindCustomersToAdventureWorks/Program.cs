42d6e39059754b959e1ac6fb539df111cf7a0e33
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-07 15:43:29 +0200

    Added some skeleton code for AdventureWorks store creation.

--- Integrations/NorthwindCustomersToAdventureWorks/NorthwindCustomersToAdventureWorks/Program.cs
+++ Integrations/NorthwindCustomersToAdventureWorks/NorthwindCustomersToAdventureWorks/Program.cs
@@ -1,15 +1,25 @@
 ﻿// Scaffold-DbContext "Server=localhost\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir NorthwindModels
 // Scaffold-DbContext "Server=localhost\SQLEXPRESS;Database=AdventureWorks;Trusted_Connection=True;Encrypt=no;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir AdventureModels
-using nw = NorthwindCustomersToAdventureWorks.NorthwindModels;
-using Microsoft.EntityFrameworkCore;
 using NorthwindCustomersToAdventureWorks.NorthwindModels;
+using nw = NorthwindCustomersToAdventureWorks.NorthwindModels;
+using NorthwindCustomersToAdventureWorks.AdventureModels;
+using aw = NorthwindCustomersToAdventureWorks.AdventureModels;
 
 Console.WriteLine("Starting to read Northwind customer information.");
 
 NorthwindContext nwContext = new();
+AdventureWorksContext awContext = new();
 foreach (nw.Customer cust in nwContext.Customers)
 {
     Console.WriteLine(cust.CompanyName);
+
+    aw.Store newStore = new()
+    {
+        // TODO: BusinessEntityId
+        Name = cust.CompanyName
+    };
+    awContext.Stores.Add(newStore);
 }
 
+awContext.SaveChanges();
 Console.WriteLine("Processing complete.");
