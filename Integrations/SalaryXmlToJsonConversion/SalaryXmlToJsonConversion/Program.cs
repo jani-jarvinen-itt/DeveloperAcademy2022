437edf17e0285a81b90defc4ca65c578ba3ac763
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-07 13:20:44 +0200

    Added USD currency rate parsing from the XML currency rate file.

--- Integrations/SalaryXmlToJsonConversion/SalaryXmlToJsonConversion/Program.cs
+++ Integrations/SalaryXmlToJsonConversion/SalaryXmlToJsonConversion/Program.cs
@@ -16,7 +16,31 @@ var employees = xmlDoc.Descendants("palkka") // LINQ to XML
                });
 
 // step 2, read currency rate from XML file from ECB
-double usdRate = 1.1313;
+filename = "..\\..\\..\\Currency rates.xml";
+xmlDoc = XDocument.Load(filename);
+XNamespace gesmes = "http://www.gesmes.org/xml/2002-08-01";
+XNamespace ns = "http://www.ecb.int/vocabulary/2002-08-01/eurofxref";
+var cubes = xmlDoc.Descendants(ns + "Cube")
+               .Where(x => x.Attribute("currency") != null)
+               .Select(x => new
+               {
+                   Currency = (string)x.Attribute("currency") ?? "",
+                   Rate = (double)x.Attribute("rate")
+               });
+
+// find the USD currency rate
+double usdRate = 0.0;
+foreach (var result in cubes)
+{
+    // Console.WriteLine($"{result.Currency}: {result.Rate}");
+    if (result.Currency == "USD")
+    {
+        usdRate = result.Rate;
+        Console.WriteLine($"Found {result.Currency} currency rate: {result.Rate}.");
+
+        break;
+    }
+}
 
 // step 3, data conversion
 List<EmployeeDestinationFormat> destinationList = new();
