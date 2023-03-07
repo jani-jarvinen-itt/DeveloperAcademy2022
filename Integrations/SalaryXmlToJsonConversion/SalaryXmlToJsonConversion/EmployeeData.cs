dd4bdb7ced17048568e5f03aabf64bdaefac9ae9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-07 11:39:30 +0200

    Started implementing salary XML to JSON conversion. Step 1 has been completed.

---
+++ Integrations/SalaryXmlToJsonConversion/SalaryXmlToJsonConversion/EmployeeData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryXmlToJsonConversion
+{
+    public class EmployeeData
+    {
+        public string Name { get; set; }
+
+        public double MonthlySalaryEuros { get; set; }
+
+        public string HireDate { get; set; }
+    }
+}
---
+++ Integrations/SalaryXmlToJsonConversion/SalaryXmlToJsonConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using SalaryXmlToJsonConversion;
+using System.Xml.Linq;
+
+Console.WriteLine("Starting salary XML to JSON conversion.");
+
+// step 1, read the salary data
+string filename = "..\\..\\..\\Salaries.xml";
+XDocument xmlDoc = XDocument.Load(filename);
+var employees = xmlDoc.Descendants("palkka") // LINQ to XML
+               .Select(x => new EmployeeData()
+               {
+                   Name = (string)x.Descendants("nimi").First(),
+                   MonthlySalaryEuros = (double)x.Descendants("kuukausittain").First(),
+                   HireDate = (string)x.Descendants("työsuhdealkoi").FirstOrDefault() ?? ""
+               });
+
+foreach (var employee in employees)
+{
+    Console.WriteLine($"{employee.Name} -> {employee.MonthlySalaryEuros}.");
+}
+
+Console.WriteLine("Conversion completed.");
