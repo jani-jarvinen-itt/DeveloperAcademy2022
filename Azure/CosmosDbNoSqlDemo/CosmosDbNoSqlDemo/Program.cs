b5ad43f1b61c4ef372c591b63ec285531f674653
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-20 15:38:21 +0200

    Added support for reading customer objects given an id.

--- Azure/CosmosDbNoSqlDemo/CosmosDbNoSqlDemo/Program.cs
+++ Azure/CosmosDbNoSqlDemo/CosmosDbNoSqlDemo/Program.cs
@@ -24,7 +24,12 @@ Container container = await database.CreateContainerIfNotExistsAsync(ContainerId
 Console.WriteLine("Connection to database is now open.");
 
 // store customer information
-await StoreCustomer(c, container);
+// await StoreCustomer(c, container);
+
+// read a customer object given an id
+const string CustomerId = "1234";
+await ReadCustomer(container, CustomerId);
+
 
 Console.WriteLine("Application closing.");
 
@@ -32,4 +37,19 @@ static async Task StoreCustomer(Customer c, Container container)
 {
     ItemResponse<Customer> response = await container.CreateItemAsync<Customer>(c, new PartitionKey(c.Id));
     Console.WriteLine($"New customer added to the database, id = {response.Resource.Id}.");
+}
+
+static async Task ReadCustomer(Container container, string customerId)
+{
+    ItemResponse<Customer> response = await container.ReadItemAsync<Customer>(customerId, new PartitionKey(customerId));
+    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        // display customer name
+        Customer readCustomer = response.Resource;
+        Console.WriteLine(readCustomer.Name);
+    }
+    else
+    {
+        Console.WriteLine($"Could not find customer with id {customerId}.");
+    }
 }
