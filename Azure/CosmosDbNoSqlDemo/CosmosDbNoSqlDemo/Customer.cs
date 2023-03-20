0bdbe952c9731da0953af99cec20338325718db3
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-20 15:17:20 +0200

    Added a custom Customer class to contain the data we want to store in Cosmos DB. Also started adding programmatic access.

---
+++ Azure/CosmosDbNoSqlDemo/CosmosDbNoSqlDemo/Customer.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CosmosDbNoSqlDemo
+{
+    public class Customer
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
--- Azure/CosmosDbNoSqlDemo/CosmosDbNoSqlDemo/Program.cs
+++ Azure/CosmosDbNoSqlDemo/CosmosDbNoSqlDemo/Program.cs
@@ -1,7 +1,26 @@
-﻿Console.WriteLine("Starting to access Cosmos DB database in Azure.");
+﻿using CosmosDbNoSqlDemo;
+using Microsoft.Azure.Cosmos;
 
+Console.WriteLine("Starting to access Cosmos DB database in Azure.");
 
+// TODO: store the primary key in a configuration file
+const string EndpointUri = "https://developeracademy.documents.azure.com:443/";
+const string PrimaryKey = "anVoQczyuCH2Gg16aUAHTTKPObRlpFBw4q66bK8fz9OjSZWqZ5zZpSQ7btU24y64m1LmCSjVdRDVACDbEfu2vw==";
 
+// define the data
+Customer c = new()
+{
+    Id = "1234",
+    Name = "John Doe",
+    Country = "Finland"
+};
+
+// create the client
+const string DatabaseId = "Customers-Jani";
+const string ContainerId = "Customers";
+CosmosClient cosmosClient = new(EndpointUri, PrimaryKey);
+Database database = await cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseId);
+Container container = await database.CreateContainerIfNotExistsAsync(ContainerId, "/id");
 
 
 Console.WriteLine("Application closing.");
