1955f0af66059f5f0b528f8fc995e7947dbe5142
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-20 13:19:36 +0200

    Added support for reading customer objects from Azure blob storage.

---
+++ Azure/BlobStorageDemo/BlobStorageDemo/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorageDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
--- Azure/BlobStorageDemo/BlobStorageDemo/Program.cs
+++ Azure/BlobStorageDemo/BlobStorageDemo/Program.cs
@@ -1,26 +1,70 @@
-﻿using Azure.Storage.Blobs;
-using System.IO;
-using System.Text;
+﻿using System.Text;
+using System.Text.Json;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using BlobStorageDemo;
 
 Console.WriteLine("Starting to access Azure Blob Storage.");
 
 // TODO: store connection string in a configuration file, not in source code
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=developeracademy;AccountKey=Fh4NnaUemJ9kwKcJhHkqJIyM00dk0RqBEfJWkvOBLfOfwqFJPkkrqmVhWpZ7s9jb8k40No4++Qj++AStTCJbQg==;EndpointSuffix=core.windows.net";
 
+/*
 // create container
 BlobContainerClient container = new(connectionString, "jani-testing");
 await container.CreateAsync();
 Console.WriteLine("Contained created.");
+*/
+
+/*
+// define data to store
+Customer c = new()
+{
+    Id = 1234,
+    Name = "John Doe",
+    Country = "Finland"
+};
 
 // upload a file
-BlobClient blob = container.GetBlobClient("my-first-blob.txt");
-string myData = "Hello, Azure from C#!";
-MemoryStream stream = new();
-byte[] bytes = Encoding.UTF8.GetBytes(myData);
-stream.Write(bytes, 0, bytes.Length);
-stream.Position = 0;
+BlobContainerClient container = new(connectionString, "jani-testing");
+string myData = JsonSerializer.Serialize(c);
+await UploadBlobData(container, "Customer.json", myData);
+*/
 
-await blob.UploadAsync(stream);
-Console.WriteLine("Upload of blob succeeded.");
+// download customer data from the blob storage
+BlobContainerClient container = new(connectionString, "jani-testing");
+Customer? c = await DownloadCustomerObject(container, "Customer.json");
 
 Console.WriteLine("Application closing.");
+
+static async Task UploadBlobData(BlobContainerClient container, string blobName, string data)
+{
+    BlobClient blob = container.GetBlobClient(blobName);
+    MemoryStream stream = new();
+    byte[] bytes = Encoding.UTF8.GetBytes(data);
+    stream.Write(bytes, 0, bytes.Length);
+    stream.Position = 0;
+
+    await blob.UploadAsync(stream);
+    Console.WriteLine("Upload of blob succeeded.");
+}
+
+static async Task<Customer?> DownloadCustomerObject(BlobContainerClient container, string jsonBlobName)
+{
+    Customer readCustomer = null!;
+
+    BlobClient blob = container.GetBlobClient(jsonBlobName);
+    Azure.Response<BlobDownloadResult> result = await blob.DownloadContentAsync();
+    if (!result.GetRawResponse().IsError)
+    {
+        readCustomer = result.Value.Content.ToObjectFromJson<Customer>();
+        Console.WriteLine(readCustomer.Name);
+    }
+    else
+    {
+        Console.WriteLine("Could not read customer object from blob storage.");
+    }
+
+    // return what we got
+    return readCustomer;
+}
