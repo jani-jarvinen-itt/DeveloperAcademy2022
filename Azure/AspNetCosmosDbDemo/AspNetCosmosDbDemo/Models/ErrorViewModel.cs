7a151cd97b561faa1324325255b99966084eb111
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-03-21 11:54:59 +0200

    Initial version of the ASP.NET MVC application for Cosmos DB demo.

---
+++ Azure/AspNetCosmosDbDemo/AspNetCosmosDbDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCosmosDbDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
